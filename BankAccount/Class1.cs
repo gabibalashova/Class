6762e2edf7225ea7ddc3f2356a2b65c750a62385
Gabriela Balashova <gabrielabalashova@gmail.com>
2026-04-26 14:46:57 +0300

    Add files via upload

---
+++ BankAccount/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        private string accountNumber;
+        private decimal balance;
+
+        public BankAccount(string accountNumber)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = 0;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+            }
+        }
+
+        public string GetInfo()
+        {
+            return ($"Сметка: {accountNumber}, Баланс: {balance}");
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount("BG123456");
+
+            acc.Deposit(1000);
+            acc.Withdraw(250);
+
+            Console.WriteLine(acc.GetInfo());
+        }
+    }
+}
