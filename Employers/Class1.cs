0b5eae81ffeed9eb4cb1092873e2d11c3de7bd50
Gabriela Balashova <gabrielabalashova@gmail.com>
2026-04-26 14:56:43 +0300

    Add files via upload

---
+++ Employers/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employers
+{
+    class Employers
+    {
+        private string name;
+        private double salary;
+        private string position;
+        private string department;
+        private string email;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Name cannot be null or empty");
+                    return;
+                }
+                name = value;
+            }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Salary cannot be less than or equal to 0");
+                    return;
+                }
+                salary = value;
+            }
+        }
+        public string Position
+        {
+            get { return position; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Position cannot be null or empty");
+                    return;
+                }
+                position = value;
+            }
+        }
+        public string Department
+        {
+            get { return department; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Department cannot be null or empty");
+                    return;
+                }
+                department = value;
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Email cannot be null or empty");
+                    return;
+                }
+                email = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Age must be greater than 0.");
+                    return;
+                }
+                age = value;
+            }
+        }
+        public Employers()
+        {
+        }
+        public Employers(string name, double salary, string position, string department, string email, int age)
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+            Department = department;
+            Email = email;
+            Age = age;
+        }
+    }
+}
---
+++ Employers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Employers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Employers> employers = new List<Employers>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                Employers employer = new Employers();
+                employer.Name = input[0];
+                employer.Salary = double.Parse(input[1]);
+                employer.Position = input[2];
+                employer.Department = input[3];
+                employer.Email = input[4];
+                if (input.Length == 5)
+                {
+                    if (input[4].Contains('@'))
+                    {
+                        employer.Email = input[4];
+                    }
+                    else
+                    {
+                        employer.Age = int.Parse(input[4]);
+                    }
+                }
+                else if (input.Length == 6)
+                {
+                    employer.Email = input[4];
+                    employer.Age = int.Parse(input[5]);
+                }
+                employers.Add(employer);
+            }
+
+        }
+    }
+}
