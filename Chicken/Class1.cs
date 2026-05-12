2fd1b3e2bf2211ab96d908587e9d52725ddb10c8
Gabriela Balashova <gabrielabalashova@gmail.com>
2026-05-12 15:23:57 +0300

    Add files via upload

---
+++ Chicken/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chicken
+{
+    internal class Chicken
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0 || value > 15)
+                {
+                    throw new ArgumentException("Age should be between 0 and 15.");
+                }
+
+                age = value;
+            }
+        }
+
+        public Chicken(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public double CalculateProductPerDay()
+        {
+            if (Age >= 0 && Age < 6)
+            {
+                return 2;
+            }
+            else if (Age >= 6 && Age < 12)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0.75;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Chicken {Name} (age {Age}) can produce {CalculateProductPerDay()} eggs per day.";
+        }
+    }
+}
---
+++ Chicken/Program.cs
@@ -0,0 +1,148 @@
+﻿namespace Chicken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 1
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Chicken chicken = new Chicken(name, age);
+
+                Console.WriteLine(chicken);
+
+                // 2
+
+
+
+                //Console.WriteLine("How many chickens? ");
+                //int n = int.Parse(Console.ReadLine());
+
+                //Chicken[] farm = new Chicken[n];
+
+                //for (int i = 0; i < n; i++)
+                //{
+                //    Console.Write("Enter name: ");
+                //    string chickenName = Console.ReadLine();
+
+                //    Console.Write("Enter age: ");
+                //    int chickenAge = int.Parse(Console.ReadLine());
+
+                //    farm[i] = new Chicken(chickenName, chickenAge);
+                //}
+
+                //foreach (Chicken ch in farm)
+                //{
+                //    Console.WriteLine(ch);
+                //}
+
+
+
+                // 3
+
+                //Console.WriteLine("Enter : ");
+                //int n = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("How many days? ");
+                //int days = int.Parse(Console.ReadLine());
+
+                //Chicken[] farm = new Chicken[n];
+
+                //double totalEggs = 0;
+
+                //for (int i = 0; i < n; i++)
+                //{
+                //    string chickenName = Console.ReadLine();
+                //    int chickenAge = int.Parse(Console.ReadLine());
+
+                //    farm[i] = new Chicken(chickenName, chickenAge);
+
+                //    totalEggs += farm[i].CalculateProductPerDay() * days;
+                //}
+                //Console.WriteLine($"Total eggs per days: {totalEggs}");
+
+
+
+                // 4
+
+                //Console.WriteLine("Enter n: ");
+                //int n = int.Parse(Console.ReadLine());
+
+                //Chicken[] farm = new Chicken[n];
+
+                //double sum = 0;
+
+                //for (int i = 0; i < n; i++)
+                //{
+                //    string chickenName = Console.ReadLine();
+                //    int chickenAge = int.Parse(Console.ReadLine());
+
+                //    farm[i] = new Chicken(chickenName, chickenAge);
+
+                //    sum += farm[i].Age;
+                //}
+
+                //double average = sum / n;
+
+                //Console.WriteLine($"{average:F2}");
+
+                // 5
+
+                //int n = int.Parse(Console.ReadLine());
+
+                //Chicken[] farm = new Chicken[n];
+
+                //for (int i = 0; i < n; i++)
+                //{
+                //    string chickenName = Console.ReadLine();
+                //    int chickenAge = int.Parse(Console.ReadLine());
+
+                //    farm[i] = new Chicken(chickenName, chickenAge);
+                //}
+                //string searchName = Console.ReadLine();
+
+                //bool found = false;
+
+                //foreach (Chicken ch in farm)
+                //{
+                //    if (ch.Name == searchName)
+                //    {
+                //        found = true;
+                //        break;
+                //    }
+                //}
+
+                //if (found)
+                //{
+                //    Console.WriteLine("Chicken found.");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Chicken not found.");
+                //}
+
+                // 6
+                //string name = Console.ReadLine();
+                //int age = int.Parse(Console.ReadLine());
+
+                //Chicken chicken = new Chicken(name, age);
+
+                //int newAge = int.Parse(Console.ReadLine());
+
+                //chicken.Age = newAge;
+
+                //Console.WriteLine(chicken);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}  
